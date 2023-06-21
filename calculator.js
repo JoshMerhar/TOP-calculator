c3fbfe274f179d84f6187d725231a6eb57a1bbbf
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2023-06-21 18:56:56 -0400

    Add addition functionality and function to clear calculator

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
-let firstNumber;
-let operation;
-let secondNumber;
-let display = document.querySelector(".display-text"); 
+let firstNumber = "";
+let operation = "";
+let secondNumber = "";
+let display = document.querySelector(".display-text");
 
 const buttons = document.querySelectorAll(".button");
 buttons.forEach(button => button.addEventListener("mouseenter", () => {
@@ -14,11 +14,22 @@ buttons.forEach(button => button.addEventListener("mouseleave", () => {
 
 const numberButtons = document.querySelectorAll(".number-button");
 numberButtons.forEach(numberButton => numberButton.addEventListener("click", () => {
-    if (display.textContent.length < 15) {
+    if (display.textContent.length < 15 && operation === "") {
         if (display.textContent === "0") {
             display.textContent = `${numberButton.textContent}`;
+            firstNumber = `${numberButton.textContent}`;
         } else {
             display.textContent += `${numberButton.textContent}`;
+            firstNumber += `${numberButton.textContent}`;
+        }
+    }
+    if (display.textContent.length < 15 && operation !== "") {
+        if (display.textContent === firstNumber || secondNumber === "0" || secondNumber === "") {
+            display.textContent = `${numberButton.textContent}`;
+            secondNumber = `${numberButton.textContent}`;
+        } else {
+            display.textContent += `${numberButton.textContent}`;
+            secondNumber += `${numberButton.textContent}`;
         }
     }
 }));
@@ -28,10 +39,41 @@ let clicked = false;
 decimalButton.addEventListener("click", () => {
     if (!clicked) {
         display.textContent += ".";
+        firstNumber += ".";
         clicked = true;
     }
 })
 
-function operate(firstNumber, operation, secondNumber) {
-    const equation = firstNumber + operation + secondNumber;
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+    display.textContent = "0";
+    firstNumber = "";
+    secondNumber = "";
+    operation = "";
+    clicked = false;
+    addClicked = false;
+});
+
+const addition = document.querySelector("#add");
+let addClicked = false;
+addition.addEventListener("click", () => {
+    if (!addClicked) {
+        operation = "+";
+        addClicked = true;
+    }
+});
+
+const solve = document.querySelector("#solve");
+solve.addEventListener("click", operate);
+
+let result;
+function operate() {
+    if (operation === "+") {
+        result = +firstNumber + +secondNumber;
+        addClicked = false;
+    }
+    display.textContent = result;
+    firstNumber = result;
+    secondNumber = "";
+    operation = "";
 }
